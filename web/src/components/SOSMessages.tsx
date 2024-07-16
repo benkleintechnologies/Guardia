55ac835941515bd31211510e9ae58a57d93b229b
Pr pL <prplevin11@gmail.com>
2024-07-16 13:45:00 +0300

    first implementation of SOS focus button on Web app

--- web/src/components/SOSMessages.tsx
+++ web/src/components/SOSMessages.tsx
@@ -1,12 +1,37 @@
 // src/components/SOSMessages.tsx
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { db } from '../firebase';
 import { collection, onSnapshot, query, where, orderBy } from 'firebase/firestore';
 import { SosData } from '../types';
 
+import * as ExpoLocation from 'expo-location';
+import MapView from 'react-native-maps';
+
+
 const SOSMessages: React.FC = () => {
   const [sosMessages, setSosMessages] = useState<SosData[]>([]);
+  const mapRef = useRef<MapView>(null);
+
+  const centerOnUser = async () => {
+    console.log("centerOnUser called (web)");
+    let { status } = await ExpoLocation.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+    }
+    console.log("mapRef: ", mapRef);
+    let location = await ExpoLocation.getCurrentPositionAsync({});
+    if (mapRef.current) {
+        console.log("center on user location: long-" + location.coords.longitude + ", lat-" + location.coords.latitude);
+        mapRef.current.animateToRegion({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+        }, 1000);
+    };
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -28,12 +53,15 @@ const SOSMessages: React.FC = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <h2>SOS Messages</h2>
-      <ul>
+      <ul className="list">
         {sosMessages.map((sos, index) => (
-          <li key={index}>
-            User ID: {sos.userId} - Team: {sos.teamId} - Time: {sos.timestamp.toLocaleString()}
+          <li key={index} className="row">
+            <div className="recordText">User ID: {sos.userId} - Team: {sos.teamId} - Time: {sos.timestamp.toLocaleString()}</div>
+            <button className="button" onClick={centerOnUser}>
+              Action
+            </button>
           </li>
         ))}
       </ul>
